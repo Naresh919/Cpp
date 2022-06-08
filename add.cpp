6c4495acd0a9e1c6addba549afbb6feddb458f2c
Naresh919 <naresh.mitta067@gmail.com>
2022-06-08 13:06:47 +0530

    Addition of 5 Numbers

---
+++ add.cpp
@@ -0,0 +1,15 @@
+//Addition of five Numbers
+#include<iostream> 
+using namespace std;
+int main() 
+{
+ int numbers[5], sum = 0;
+ cout << "Enter 5 numbers:\n";
+ for (int i = 0; i < 5; ++i) 
+ {
+ cin >> numbers[i];
+ sum += numbers[i];
+ }
+ cout << "Sum = " << sum << endl; 
+ return 0;
+}
