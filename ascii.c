ca219ed3720874cccf97c7544815fad5dc6afff8
Naresh919 <naresh.mitta067@gmail.com>
2022-06-14 11:36:18 +0530

    Find the ASCII Number

---
+++ ascii.c
@@ -0,0 +1,15 @@
+//find ASCII Number
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\tFind The ASCII Number\n\n\n");
+
+    char c;
+    printf("Enter a character : ");
+    scanf("%c" , &c);
+    printf("\n\nASCII value of %c = %d",c,c);
+
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
